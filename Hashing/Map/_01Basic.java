928b7a6fe5c6708b4f1579cd53e3ce7da2c466a2
prasadr201 <rakhiprasad200@gmail.com>
2024-10-01 14:27:57 +0000

    hm

---
+++ Hashing/Map/_01Basic.java
@@ -0,0 +1,48 @@
+//Map -> take input in 'pair' form (key, value)
+// give output => unorderd
+/* Operation => O(1)
+ * 1. put(key,value)   2. get(key)  3.containsKey(key) => (is present)   4.remove(key)
+ */
+
+//1.hash Map
+import java.util.*;
+public class _01Basic{
+    public static void main(String[] args) {
+        HashMap<String,Integer> hm = new HashMap<>();
+        hm.put("india",30);
+        hm.put("china",56);
+        hm.put("bhutan",48);
+        hm.put("pak", 10);
+
+        System.out.println(hm);
+        System.out.println(hm.get("bhutan"));
+
+        hm.remove("china");
+        
+        System.out.println(hm);
+
+        System.out.println(hm.containsKey("india"));
+        // hm.clear();
+        
+        // System.out.println(hm);
+
+
+        //iteration
+        //use => keySet
+        Set<String> key = hm.keySet();
+        System.out.println(key);
+
+        //or
+        Set<String> key1 = hm.keySet();
+        for(String k: key1){
+            // System.out.println(k);  Or for values 
+            System.out.println(k + "  , value = " + hm.get(k));
+            
+        }
+
+
+        //print with values => emptySet()
+        
+
+    }
+}
---
+++ Hashing/_01Basic.java
@@ -0,0 +1,6 @@
+//Hashing
+/*
+ 1. convert one data into another
+ 2. type => 1.Map -> 1.Hash map  2. Linked Hash map  3. tree map
+            2.Set -> 1.Hash set  2. Linked Hash set  3. tree set
+ */
