e1529c13f37a7b8857468a7216d106fc852c39ea
prasadr201 <rakhiprasad200@gmail.com>
2024-10-19 04:43:23 +0000

    STRING COMPARISION

--- a.java
+++ a.java
@@ -1,19 +1,24 @@
 import java.util.*;
 public class a{
 
-    public static int factorial(int a){
-        int fact = 1;
-        while(a>0){
-            fact *= a;
-            a--;
+    public static boolean isPalindrom(String s, int i, int j){
+        while(i<=j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++; j--;
         }
-        return fact;
+        return true;
     }
 
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println(factorial(a));
-
+        String s = sc.next();
+        if(isPalindrom(s,0,s.length()-1) == true){
+            System.out.println("palindrom");
+        }
+        else{
+            System.out.println("not a  palindrom");
+        }
     }
 }
--- string/equals.java
+++ string/equals.java
@@ -25,3 +25,39 @@ class equals
 
 	}
 }
+
+
+
+/* array of string compair whith a character */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int j=0; j<t; j++){
+		    String s[] = new String[6];
+		    for(int i=0; i<6; i++){
+		        s[i] = sc.next();
+		    }
+		    
+		    boolean isHatric = false;
+		    int i=0; 
+		    while(i<6){
+		        if(i<=3 && s[i].equals("W") && s[i+1].equals("W") && s[i+2].equals("W")){
+		            isHatric = true;
+		            break;
+		        }
+		        i++;
+		}
+		
+		System.out.println((isHatric == true)?"yes":"no");
+
+	}
+}
+}
